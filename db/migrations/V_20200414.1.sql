dde1352493f7c3805f7a4af19b1ff69403b5ffea
etiennebch <et.bouche@gmail.com>
2020-04-16 13:53:46 +0200

    Create role and grant privileges to api role

--- db/migrations/V_20200414.1.sql
+++ db/migrations/V_20200414.1.sql
@@ -1,3 +1,20 @@
+CREATE ROLE ${api_rolename} NOSUPERUSER NOCREATEDB NOCREATEROLE LOGIN ENCRYPTED PASSWORD ${api_password};
+
+CREATE DATABASE ${database_name} ENCODING 'UTF-8';
+
 /* the btc schema stores information about the Bitcoin blockchain.
 **/
 CREATE SCHEMA btc;
+
+/* grant privileges
+**/
+GRANT CONNECT ON DATABASE ${database_name} TO ${api_rolename};
+GRANT USAGE ON SCHEMA btc TO ${api_rolename};
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA btc TO ${api_rolename};
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA btc TO ${api_rolename};
+
+/* grant default privileges for future objects
+**/
+ALTER DEFAULT PRIVILEGES FOR ${api_rolename} IN SCHEMA btc GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO ${api_rolename};
+ALTER DEFAULT PRIVILEGES FOR ${api_rolename} IN SCHEMA btc USAGE ON SEQUENCES TO ${api_rolename};
