09026b3f3909877df81a9867a571d6b96e82e02e
etiennebch <et.bouche@gmail.com>
2020-04-17 23:58:23 +0200

    Store nbits as bytea

--- db/migrations/V20200416.1.sql
+++ db/migrations/V20200416.1.sql
@@ -26,10 +26,12 @@ CREATE TABLE btc.block (
     merkle_root_hash BYTEA NOT NULL,
     -- we don't care about the time zone as the timestamp is always obtained as seconds since unix epoch.
     block_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    -- we use BIGINT for nbits and nonce since Bitcoin core uses unsigned 32bits integers and PostgreSQL does not
-    -- support unsigned integers. nbits is the compact form of the difficulty target
+    -- nbits is the compact form of the block target. Bitcoin uses unsigned int32.
+    -- as the position of each byte is critical we use bytea here to store the data.
     -- https://en.bitcoin.it/wiki/Difficulty#How_is_difficulty_stored_in_blocks.3F
-    nbits BIGINT NOT NULL,
+    nbits BYTEA NOT NULL,
+    -- we use BIGINT for nonce since Bitcoin core uses unsigned 32bits integers and PostgreSQL does not
+    -- support unsigned integers.
     nonce BIGINT NOT NULL,
 
     /* mining data
