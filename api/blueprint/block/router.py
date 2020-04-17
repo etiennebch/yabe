61881a431f52968f7f1430a01a3293770d5d1267
etiennebch <et.bouche@gmail.com>
2020-04-18 00:44:28 +0200

    Fix url for the block blueprint

--- api/blueprint/block/router.py
+++ api/blueprint/block/router.py
@@ -5,6 +5,6 @@ from flask import Blueprint
 
 from api.blueprint.block import endpoint
 
-block = Blueprint("block", __name__, url_prefix="/block")
+block = Blueprint("block", __name__, url_prefix="/blocks")
 
 block.add_url_rule("/<int:block_id>", endpoint="retrieve", methods=["GET"], view_func=endpoint.retrieve)
