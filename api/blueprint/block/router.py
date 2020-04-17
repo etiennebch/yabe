1fbd35e64f8024bf65b7721c088bf2c832d6a6e5
etiennebch <et.bouche@gmail.com>
2020-04-17 14:42:41 +0200

    Convert block_id path parameter to integer instead of string

--- api/blueprint/block/router.py
+++ api/blueprint/block/router.py
@@ -3,4 +3,8 @@ The block blueprint exposes endpoints about individual blocks on the blockchain.
 """
 from flask import Blueprint
 
+from api.blueprint.block import endpoint
+
 block = Blueprint("block", __name__, url_prefix="/block")
+
+block.add_url_rule("/<int:block_id>", endpoint="retrieve", methods=["GET"], view_func=endpoint.retrieve)
