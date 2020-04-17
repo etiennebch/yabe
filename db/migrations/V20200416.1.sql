c163a8331cacff43a2a22f804486aa545ef7e9b2
etiennebch <et.bouche@gmail.com>
2020-04-17 14:40:43 +0200

    Use sequence to autoincrement id
    Use a sequence to autoincrement block.id and define the primary key as a table constraint for explicit naming.

--- db/migrations/V20200416.1.sql
+++ db/migrations/V20200416.1.sql
@@ -2,7 +2,7 @@
 **/
 CREATE TABLE btc.block (
     
-    id INTEGER NOT NULL PRIMARY KEY,
+    id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1001 NO CYCLE),
     block_hash BYTEA NOT NULL,
 
     -- the size of the block in bytes. A block is valid under consensus rules if less than 1Mb.
@@ -27,7 +27,8 @@ CREATE TABLE btc.block (
     -- we don't care about the time zone as the timestamp is always obtained as seconds since unix epoch.
     block_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     -- we use BIGINT for nbits and nonce since Bitcoin core uses unsigned 32bits integers and PostgreSQL does not
-    -- support unsigned integers.
+    -- support unsigned integers. nbits is the compact form of the difficulty target
+    -- https://en.bitcoin.it/wiki/Difficulty#How_is_difficulty_stored_in_blocks.3F
     nbits BIGINT NOT NULL,
     nonce BIGINT NOT NULL,
 
@@ -44,7 +45,9 @@ CREATE TABLE btc.block (
     -- the value in satoshi of all inputs of this block's transactions
     block_input BIGINT NOT NULL,
     -- the value in satoshi of all outputs of this block's transactions. 
-    block_output BIGINT NOT NULL
+    block_output BIGINT NOT NULL,
+
+    CONSTRAINT pk_block__block_id PRIMARY KEY (id)
 );
 
 CREATE INDEX idx_block__block_hash ON btc.block USING HASH (block_hash);
