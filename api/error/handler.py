f6c5e568ee171f9a93ac6d1bc912a268d68a6c9a
etiennebch <et.bouche@gmail.com>
2020-04-22 16:56:34 +0200

    Add logging statement in error handler

--- api/error/handler.py
+++ api/error/handler.py
@@ -2,7 +2,7 @@
 """
 from http import HTTPStatus
 
-from flask import jsonify
+from flask import current_app, jsonify
 from werkzeug.exceptions import HTTPException, InternalServerError
 
 from api.error.definition import BaseError
@@ -17,7 +17,16 @@ def jsonify_error_handler(exception):
     :rtype: flask.Response.
     """
     if isinstance(exception, BaseError):
+        if exception.status >= HTTPStatus.INTERNAL_SERVER_ERROR:
+            current_app.logger.exception(exception.message)
+        else:
+            current_app.logger.warning(exception.message)
         return jsonify(exception), exception.status
     if isinstance(exception, HTTPException):
+        if exception.code >= HTTPStatus.INTERNAL_SERVER_ERROR:
+            current_app.logger.exception(exception.description)
+        else:
+            current_app.logger.warning(exception.description)
         return jsonify(exception), exception.code
+    current_app.logger.exception(str(exception))
     return jsonify(InternalServerError()), HTTPStatus.INTERNAL_SERVER_ERROR
