53e60164d90855b96818017608c24be7d639379f
etiennebch <et.bouche@gmail.com>
2020-04-23 18:46:47 +0200

    Fix healthcheck with new context manager cursor

--- api/blueprint/healthcheck/endpoint.py
+++ api/blueprint/healthcheck/endpoint.py
@@ -8,10 +8,11 @@ from api.database import db
 def ping():
     """Ping the api server and the underlying database to make sure it is alive.
     """
-    try:
-        db.cursor.execute("SELECT 1")
-    except Exception as e:
-        current_app.logger.exception(e)
-        return ("", 503)
+    with db.cursor() as cursor:
+        try:
+            cursor.execute("SELECT 1")
+        except Exception as e:
+            current_app.logger.exception(e)
+            return ("", 503)
     current_app.logger.info("healthcheck passed")
     return ("", 200)
